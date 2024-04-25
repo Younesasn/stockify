0351677807b4396ffaa18086aa9a943ca50405d5
ASSANI Younes <younesassani@gmail.com>
2024-04-26 00:27:50 +0200

    add Upload in Dashboard

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Upload;
+use App\Form\UploadType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class IndexController extends AbstractController
 {
@@ -15,8 +21,49 @@ class IndexController extends AbstractController
     }
 
     #[Route('/dashboard', name: 'dashboard')]
-    public function dashboard(): Response
+    public function dashboard(Request $request, SluggerInterface $slugger, EntityManagerInterface $em): Response
     {
-        return $this->render('index/dashboard.html.twig');
+        $file = new Upload();
+        $form = $this->createForm(UploadType::class, $file);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $brochureFile = $form->get('filename')->getData();
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $file->setExtension(pathinfo($brochureFile->getClientOriginalName(), PATHINFO_EXTENSION));
+                $file->setSize(filesize($this->getParameter('uploads_directory') . '/' . $newFilename));
+                $file->setOriginalFilename($brochureFile->getClientOriginalName());
+                $file->setFilename($newFilename);
+            }
+
+            $file->setDate(new \DateTime());
+            $em->persist($file);
+            $em->flush();
+
+            $this->addFlash('success', $file->getOriginalFilename() . ' a bien été enregistée dans ' . $file->getFolder());
+            return $this->redirectToRoute('dashboard');
+        }
+        return $this->render('index/dashboard.html.twig', [
+            'form'=> $form
+        ]);
     }
 }
--- src/Form/UploadType.php
+++ src/Form/UploadType.php
@@ -7,6 +7,8 @@ use App\Entity\Folder;
 use App\Entity\Upload;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,20 +17,24 @@ class UploadType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('filename')
-            ->add('size')
-            ->add('extension')
-            ->add('originalFilename')
-            ->add('date', null, [
-                'widget' => 'single_text',
-            ])
+            ->add('filename', FileType::class)
+            // ->add('size')
+            // ->add('extension')
+            // ->add('originalFilename')
+            // ->add('date', null, [
+            //     'widget' => 'single_text',
+            // ])
             ->add('folder', EntityType::class, [
                 'class' => Folder::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyez', 
+                'attr' => ['class'=> 'bg-secondary'],
             ])
         ;
     }
